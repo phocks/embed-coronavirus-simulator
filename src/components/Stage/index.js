1dc58058d4edc759995543e8e07a41d920259ad4
Joshua Byrd <phocks@gmail.com>
2020-04-02 13:40:40 +1000

    Grow in place

--- src/components/Stage/index.js
+++ src/components/Stage/index.js
@@ -58,20 +58,20 @@ simulation = d3
       .radius(d => d.size)
       .iterations(1)
   )
-  .force(
-    'x',
-    d3
-      .forceX()
-      .strength(0.002)
-      .x(d => d.targetX)
-  )
-  .force(
-    'y',
-    d3
-      .forceY()
-      .strength(0.002)
-      .y(d => d.targetY)
-  )
+  // .force(
+  //   "x",
+  //   d3
+  //     .forceX()
+  //     .strength(0.002)
+  //     .x(d => d.targetX)
+  // )
+  // .force(
+  //   "y",
+  //   d3
+  //     .forceY()
+  //     .strength(0.002)
+  //     .y(d => d.targetY)
+  // )
   .alpha(1)
   .alphaDecay(0.0228)
   .alphaMin(0.001)
@@ -130,66 +130,67 @@ const Stage = props => {
     canvas = d3.select(canvasEl.current);
     context = canvas.node().getContext("2d");
 
-    animate = time => {
-      // console.log(simulation.alpha());
-      if (!startTime) {
-        startTime = time;
-      }
+    // This is now handled in the simplified function
+    // animate = time => {
+    //   // console.log(simulation.alpha());
+    //   if (!startTime) {
+    //     startTime = time;
+    //   }
 
-      const progress = time - startTime;
-      const nodes = render(simulation);
-      const newNodes = [];
+    //   const progress = time - startTime;
+    //   const nodes = render(simulation);
+    //   const newNodes = [];
 
-      for (let i = 0; i < nodesToAdd.length; i++) {
-        const node = nodesToAdd[i];
-        if (node.delay < progress) {
-          // Here we are simulating new dots "dividing" from dots already there
-          // So we randomly select nodes until we find one that matches
+    //   for (let i = 0; i < nodesToAdd.length; i++) {
+    //     const node = nodesToAdd[i];
+    //     if (node.delay < progress) {
+    //       // Here we are simulating new dots "dividing" from dots already there
+    //       // So we randomly select nodes until we find one that matches
 
-          // Make an array of random numbers
-          // and go through until we find one
-          for (var a = [], j = 0; j < nodes.length; ++j) a[j] = j;
-          const shuffledArray = shuffle(a);
+    //       // Make an array of random numbers
+    //       // and go through until we find one
+    //       for (var a = [], j = 0; j < nodes.length; ++j) a[j] = j;
+    //       const shuffledArray = shuffle(a);
 
-          for (let randomNumber of shuffledArray) {
-            if (nodes[randomNumber].groupName === node.groupName) {
-              node.y = nodes[randomNumber].y;
-              node.x = nodes[randomNumber].x;
-              break;
-            }
-          }
+    //       for (let randomNumber of shuffledArray) {
+    //         if (nodes[randomNumber].groupName === node.groupName) {
+    //           node.y = nodes[randomNumber].y;
+    //           node.x = nodes[randomNumber].x;
+    //           break;
+    //         }
+    //       }
 
-          newNodes.push(node);
-          nodesToAdd.splice(i, 1);
-          i--;
-        }
-      }
+    //       newNodes.push(node);
+    //       nodesToAdd.splice(i, 1);
+    //       i--;
+    //     }
+    //   }
 
-      simulation.nodes(nodes.concat(newNodes)).tick();
+    //   simulation.nodes(nodes.concat(newNodes)).tick();
 
-      ticks++;
+    //   ticks++;
 
-      if (
-        (runAnimation && ticks < ANIMATION_TICK_LIMIT) ||
-        nodesToAdd.length > 0
-      ) {
-        isAnimating = true;
+    //   if (
+    //     (runAnimation && ticks < ANIMATION_TICK_LIMIT) ||
+    //     nodesToAdd.length > 0
+    //   ) {
+    //     isAnimating = true;
 
-        if (FPS < 60) {
-          setTimeout(() => {
-            requestAnimationFrame(t => {
-              animate(t, nodesToAdd); // nodesToAdd doesn't matter for now
-            });
-          }, 1000 / FPS);
-        } else {
-          requestAnimationFrame(t => {
-            animate(t, nodesToAdd); // nodesToAdd doesn't matter for now
-          });
-        }
-      } else {
-        isAnimating = false;
-      }
-    };
+    //     if (FPS < 60) {
+    //       setTimeout(() => {
+    //         requestAnimationFrame(t => {
+    //           animate(t, nodesToAdd); // nodesToAdd doesn't matter for now
+    //         });
+    //       }, 1000 / FPS);
+    //     } else {
+    //       requestAnimationFrame(t => {
+    //         animate(t, nodesToAdd); // nodesToAdd doesn't matter for now
+    //       });
+    //     }
+    //   } else {
+    //     isAnimating = false;
+    //   }
+    // };
 
     const response = await getData(
       "https://www.abc.net.au/dat/news/interactives/covid19-data/hybrid-country-totals.json"
@@ -238,40 +239,62 @@ const Stage = props => {
   useEffect(() => {
     if (typeof data === "null") return;
 
-    let dots = [];
+    let nodes = simulation.nodes();
 
-    // startTime = false;
-    // ticks = 0;
+    // for (const countryName in data) {
+    //   const count = data[countryName][date.format(DATE_FORMAT)];
+    //   if (typeof count === "undefined" || count === 0) continue;
 
-    // for (let i = 0; i < 20; i++) {
+    //   dots.push({
+    //     groupName: "one",
+    //     x:
+    //       windowWidth / 2
+    //       +
+    //       (Math.random() * RANDOM_INIT_DISTANCE - RANDOM_INIT_DISTANCE / 2),
+    //     y:
+    //       windowHeight * 0.5
+    //       +
+    //       (Math.random() * RANDOM_INIT_DISTANCE - RANDOM_INIT_DISTANCE / 2),
+    //     targetX: windowWidth / 2,
+    //     targetY: windowHeight * 0.5,
+    //     size: calculateRadius(count)
+    //   });
+    // }
+
     for (const countryName in data) {
       const count = data[countryName][date.format(DATE_FORMAT)];
       if (typeof count === "undefined" || count === 0) continue;
 
-      dots.push({
-        groupName: "one",
-        x:
-          windowWidth / 2 
-          +
-          (Math.random() * RANDOM_INIT_DISTANCE - RANDOM_INIT_DISTANCE / 2),
-        y:
-          windowHeight * 0.5 
-          +
-          (Math.random() * RANDOM_INIT_DISTANCE - RANDOM_INIT_DISTANCE / 2),
-        targetX: windowWidth / 2,
-        targetY: windowHeight * 0.5,
-        size: calculateRadius(count)
-      });
-    }
+      let shouldAdd = true;
 
-    
+      for (const node of nodes) {
+        if (node.name === countryName) {
+          node.size = calculateRadius(count);
+          shouldAdd = false;
+        }
+      }
+
+      if (shouldAdd) {
+        nodes.push({
+          name: countryName,
+          x:
+            windowWidth / 2 +
+            (Math.random() * RANDOM_INIT_DISTANCE - RANDOM_INIT_DISTANCE / 2),
+          y:
+            windowHeight * 0.5 +
+            (Math.random() * RANDOM_INIT_DISTANCE - RANDOM_INIT_DISTANCE / 2),
+          targetX: windowWidth / 2,
+          targetY: windowHeight * 0.5,
+          size: calculateRadius(count)
+        });
+      }
+    }
 
     if (isAnimating()) {
       // simulation.nodes(simulation.nodes().concat(dots)).alpha(1.0);
-      simulation.nodes(dots).alpha(1.0);
+      simulation.nodes(nodes).alpha(1.0);
     } else {
-
-      simulation.nodes(dots).alpha(1.0);
+      simulation.nodes(nodes).alpha(1.0);
 
       requestAnimationFrame(t => {
         processFrame(t);
